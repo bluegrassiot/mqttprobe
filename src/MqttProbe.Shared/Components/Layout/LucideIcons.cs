b48152e5edb15a1128a418536137aa9be4a94ff1
Mark Todd <mark.todd@bluegrassiot.com>
2026-07-26 15:05:07 -0400

    refactor: updated settings icon for sparkplug & fixed icon alignment

--- src/MqttProbe.Shared/Components/Layout/LucideIcons.cs
+++ src/MqttProbe.Shared/Components/Layout/LucideIcons.cs
@@ -37,7 +37,7 @@ public static class LucideIcons
     // Connectivity / MQTT
     public const string Wifi           = $"<path {S} d='M5 12.55a11 11 0 0 1 14.08 0M1.42 9a16 16 0 0 1 21.16 0M8.53 16.11a6 6 0 0 1 6.95 0'/>";
     public const string WifiOff        = $"<path {S} d='M1.42 9a16 16 0 0 1 21.16 0M5 12.55a11 11 0 0 1 14.08 0M8.53 16.11a6 6 0 0 1 6.95 0M1 1l22 22'/>";
-    public const string Radio          = $"<path {S} d='M18.89 8.11a7 7 0 0 0-9.9 0M15.46 10.54a4 4 0 0 0-5.66 0'/>";
+    public const string Radio          = $"<path {S} d='M7.75 16.25a6 6 0 0 1 0-8.49M16.25 7.76a6 6 0 0 1 0 8.49M4.93 19.07a10 10 0 0 1 0-14.14M19.07 4.93a10 10 0 0 1 0 14.14'/><circle {S} cx='12' cy='12' r='2'/>";
     public const string Hash           = $"<path {S} d='M4 9h16M4 15h16M10 3L8 21M16 3l-2 18'/>";
     public const string GitHub         = $"<path {S} d='M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4M9 18c-4.51 2-5-2-7-2'/>";
 
